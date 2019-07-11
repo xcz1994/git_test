db4bced256cefaf25ad76c6089e5b1b1731b4f82
xcz <854481516@qq.com>
2019-07-11 10:00:18 +0800

    把本地工程添加到git

---
+++ git_test_project/src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,9 @@
+package cn.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
