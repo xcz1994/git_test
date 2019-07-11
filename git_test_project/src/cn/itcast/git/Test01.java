ad1689da12e5f5f5b932b305a8c720fc33265212
xcz <854481516@qq.com>
2019-07-11 10:57:44 +0800

    增加了一个for循环

--- git_test_project/src/cn/itcast/git/Test01.java
+++ git_test_project/src/cn/itcast/git/Test01.java
@@ -3,5 +3,9 @@ package cn.itcast.git;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("球球");
+        //------------------------
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
